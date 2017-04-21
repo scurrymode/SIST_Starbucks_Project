25ed1d70aaceac3ef8c135340cfafae7b85bb5cf
DESKTOP-I8P6V4B\user <impactorz@gmail.com>
2017-04-20 18:20:25 -0700

    지민: 재고관리페이지에서 엑셀 다운로드 및 엑셀 업로드 기능 완성~ 디비에 때려박는건 아직 안함

--- src/pos/GoodsPanel.java
+++ src/pos/GoodsPanel.java
@@ -1,26 +1,47 @@
 package pos;
 
 import java.awt.BorderLayout;
-import java.awt.Choice;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Vector;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.AbstractTableModel;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 public class GoodsPanel extends MyPanel implements ActionListener,TableModelListener{
 	JTextField t_search;
 	JLabel la_name;
 	JButton bt_search,bt_reg,bt_edit;
+	//엑셀 버튼 바닥에 붙일꺼~!
+	JButton bt_excel_download, bt_excel_upload;
+	JFileChooser chooser = new JFileChooser("");
+	
+	Vector<String> columnName;
+	Vector<Vector> data;
+	Vector<String> vec;
+	
 
 	public GoodsPanel() {
 		t_search = new JTextField(20);
@@ -37,11 +58,18 @@ public class GoodsPanel extends MyPanel implements ActionListener,TableModelList
 		//버튼에 리스너 연결
 		bt_reg.addActionListener(this);
 		//테이블 초기 설정
-		model =(DataModel) dataController.getDataModel();
+		model =(DataModel)dataController.getDataModel();
 		model.addTableModelListener(this);
-		
-		
+			
 		table.setModel(model);
+		
+		//엑셀관련 디자인
+		bt_excel_download = new JButton("엑셀파일로 받기");
+		bt_excel_upload = new JButton("엑셀파일 올리기");
+		p_south.add(bt_excel_download);
+		p_south.add(bt_excel_upload);
+		bt_excel_download.addActionListener(this);
+		bt_excel_upload.addActionListener(this);
 		
 		add(p_north,BorderLayout.NORTH);
 		add(scroll);
@@ -51,12 +79,127 @@ public class GoodsPanel extends MyPanel implements ActionListener,TableModelList
 		Object obj = e.getSource();
 		if(obj == bt_reg){
 			regist();
+		}else if(obj == bt_excel_download){
+			downLoad();
+		}else if(obj == bt_excel_upload){
+			upLoad();
+		}		
+	}
+	//테이블의 내용을 엑셀 파일에 받기!
+	public void downLoad(){
+		//선택한 디렉토리겟
+		chooser.setFileFilter(new FileNameExtensionFilter("xls", "xls"));
+		int result = chooser.showSaveDialog(this);
+		File file = chooser.getSelectedFile();
+		
+		if(result==JFileChooser.APPROVE_OPTION){
+			Workbook wb = new HSSFWorkbook();
+		    Sheet sheet = wb.createSheet("goods");
+		    
+		    int table_row=table.getRowCount();
+		    int table_col=table.getColumnCount();
+		  
+		    
+		    for(int a=0; a<table_row+1;a++){
+			    Row row = sheet.createRow((short)a);
+			    for(int i=0; i<table_col;i++){
+			    	String value=null;
+			    	Cell cell = row.createCell(i);
+			    	if(a==0){
+			    		value = table.getColumnName(i);
+			    	}else {
+			    		value = (String)table.getValueAt(a-1, i);
+			    	}
+			    	cell.setCellValue(value);
+			    }
+		    }
+		    try {
+				FileOutputStream fos = new FileOutputStream(file+".xls");
+				wb.write(fos);
+				fos.close();
+				JOptionPane.showMessageDialog(this, "엑셀 다운로드 완료");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
+	
+	public void upLoad(){
+		//파일 업로드하기!
+		int result = chooser.showOpenDialog(this);
+		File file = chooser.getSelectedFile();
+		//컬럼네임용 벡터
+		columnName = new Vector<String>();
+		//컬럼데이터용 벡터
+		data = new Vector<Vector>();
+		
+		if(result==JFileChooser.APPROVE_OPTION){
+			try {
+				FileInputStream fis = new FileInputStream(file);
+				HSSFWorkbook wb = new HSSFWorkbook(fis);
+				//시트가져오기!
+				HSSFSheet sheet = wb.getSheetAt(0);
+				for(int a=0;a<sheet.getLastRowNum()+1;a++){
+					HSSFRow row = sheet.getRow(a);
+					
+					if(a!=0){
+						vec = new Vector<String>();
+					}
+					for(int i=0; i<row.getLastCellNum();i++){
+						//컬럼네임 따로 모으자!
+						if(a==0){
+							HSSFCell col = row.getCell(i);
+							columnName.add(col.getStringCellValue());
+						} else {
+							HSSFCell col = row.getCell(i);
+							if(col.getCellType()==HSSFCell.CELL_TYPE_NUMERIC){
+								vec.add(Integer.toString(Double.valueOf(col.getNumericCellValue()).intValue()));
+
+							}else if(col.getCellType()==HSSFCell.CELL_TYPE_STRING){
+								vec.add(col.getStringCellValue());
+							}
+						}
+					}
+					//이제 다 담자~!
+					if(a!=0){
+						data.add(vec);
+					}
+				}
+				//테이블 다시 만들기!
+				table.setModel(new AbstractTableModel() {
+					public int getRowCount() {
+						return GoodsPanel.this.data.size();
+					}
+					public int getColumnCount() {
+						return GoodsPanel.this.columnName.size();
+					}
+					public String getColumnName(int col) {
+						return GoodsPanel.this.columnName.get(col);
+					}
+					public Object getValueAt(int rowIndex, int columnIndex) {
+						return GoodsPanel.this.data.get(rowIndex).get(columnIndex);
+					}
+
+				});
+				table.updateUI();
+				
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		//나중에 디비 변경하는거 추가해야함~~!
+	}
+	
+	
 	public void regist(){
 		//System.out.println(dataController.data.get(1).get(1));
 		JOptionPane.showMessageDialog(this, "회원 계정 등록");
 	}
+	
 	public void tableChanged(TableModelEvent e) {
 		int row = e.getFirstRow();
 		int col = e.getColumn();
