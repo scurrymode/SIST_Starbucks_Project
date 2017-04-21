e5d4254faa255981267b919e9f9aab290e82025f
user-pc\user <kma6653805@gmail.com>
2017-04-21 14:06:13 +0900

    로그인폼 회원가입폼 디자인완료

--- src/member/JoinForm.java
+++ src/member/JoinForm.java
@@ -1,15 +1,26 @@
 package member;
 
+import java.awt.Canvas;
 import java.awt.Choice;
-import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -18,21 +29,25 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 import db.DBManager;
 
 public class JoinForm extends JPanel implements ActionListener {
 
 	MemberWindow memberWindow;
-	JLabel label[] = new JLabel[11]; // Label 배열로 선언
-	JPanel p1, p2, p3, p4, p5, p6, p7, p8, p9, p10;
-	JLabel log, id_la, pw_la, nickname_la, name_la, gender_la, phone_la, phone_la1, phone_la2, birth_la, birth_la1;
+	JPanel p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p_can, p_can2;
+	JLabel log, log2, pw_la, name_la, nickname_la, gender_la, phone_la, phone_la1, phone_la2, birth_la, birth_la1;
 	JTextField id_t, nickname_t, name_t, phone_t2, phone_t3, birth_t;
 	JPasswordField pw_t;
 	JButton id_bt, bt_trans, bt_cancel;
 	ButtonGroup gender;
 	JRadioButton female, male;
 	Choice phone_t1;
+
+	Canvas can_top, can_nick;
+	BufferedImage image = null;
+	BufferedImage image2 = null;
 
 	Member member;
 
@@ -43,6 +58,7 @@ public class JoinForm extends JPanel implements ActionListener {
 
 	public JoinForm(MemberWindow memberWindow) {
 		this.memberWindow = memberWindow;
+
 		p1 = new JPanel();
 		p2 = new JPanel();
 		p3 = new JPanel();
@@ -53,37 +69,107 @@ public class JoinForm extends JPanel implements ActionListener {
 		p8 = new JPanel();
 		p9 = new JPanel();
 		p10 = new JPanel();
+		p_can = new JPanel();
+		p_can2 = new JPanel();
+
+		Border border = BorderFactory.createLineBorder(Color.GRAY);
+		p1.setBorder(border);
+		p2.setBorder(border);
+		p3.setBorder(border);
+		p4.setBorder(border);
+		p5.setBorder(border);
+		p6.setBorder(border);
+		p7.setBorder(border);
+		p8.setBorder(border);
 
 		// title
-		log = new JLabel("**********회원가입**********");
+		p1.setBackground(Color.BLACK);
+		log = new JLabel("회원가입");
+		log.setForeground(Color.WHITE);
+		p1.setLayout(null);
+		log.setBounds(10, 0, 100, 55);
+
 		p1.add(log);
 		add(p1);
 
+		p1.setBorder(border);
+
+		// p_can
+		p_can.setBackground(Color.WHITE);
+		log2 = new JLabel("회원정보를 입력해 주세요.", JLabel.CENTER);
+		// 이미지 url 얻어오기
+		try {
+			URL url = this.getClass().getResource("/icon_find_sally.png");
+			image = ImageIO.read(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		can_top = new Canvas() {
+			public void paint(Graphics g) {
+				g.drawImage((Image) image, 0, 5, 140, 130, this);
+
+			}
+		};
+		can_top.setPreferredSize(new Dimension(140, 130));
+
+		p_can.add(can_top);
+		p_can.add(log2);
+		add(p_can);
+
+		p_can.setLayout(null);
+		can_top.setBounds(135, 10, 400, 130);
+		log2.setBounds(0, 150, 400, 17);
+
 		// ID
-		id_la = new JLabel("ID : ");
-		p2.add(id_la);
-		id_t = new JTextField("", 15);
+		id_t = new JTextField("아이디", 15);
+		id_t.addMouseListener(new MyMouseListener());
 		p2.add(id_t);
 		id_bt = new JButton("아이디 중복 확인");
 		p2.add(id_bt);
 		add(p2);
 
 		// Password
-		pw_la = new JLabel("Password : ");
+		pw_la = new JLabel("비밀번호");
 		p3.add(pw_la);
-		pw_t = new JPasswordField("", 15);
+		pw_t = new JPasswordField("비밀번호", 15);
+		pw_t.addMouseListener(new MyMouseListener());
 		p3.add(pw_t);
 		add(p3);
 
 		// 이름
-		name_la = new JLabel("이름 : ");
+		name_la = new JLabel("이름");
 		p4.add(name_la);
 		name_t = new JTextField("", 15);
 		p4.add(name_t);
 		add(p4);
 
+		// p_can2
+		p_can2.setBackground(Color.WHITE);
+		// 이미지 url 얻어오기
+		try {
+			URL url = this.getClass().getResource("/nick.png");
+			image2 = ImageIO.read(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		can_nick = new Canvas() {
+			public void paint(Graphics g) {
+				g.drawImage((Image) image2, 0, 5, 400, 190, this);
+
+			}
+		};
+		can_nick.setPreferredSize(new Dimension(400, 200));
+
+		p_can2.add(can_nick);
+		add(p_can2);
+
+		p_can2.setLayout(null);
+		can_nick.setBounds(10, 0, 380, 200);
+
 		// 닉네임
-		nickname_la = new JLabel("닉네임 : ");
+		nickname_la = new JLabel("닉네임");
 		p5.add(nickname_la);
 		nickname_t = new JTextField("", 15);
 		p5.add(nickname_t);
@@ -125,15 +211,20 @@ public class JoinForm extends JPanel implements ActionListener {
 
 		// 생년월일
 		birth_la = new JLabel("생년월일");
+		id_t.addMouseListener(new MyMouseListener());
 		p8.add(birth_la);
-		birth_t = new JTextField("", 15);
+		birth_t = new JTextField("ex) 19940621", 15);
 		p8.add(birth_t);
-		birth_la1 = new JLabel("ex) 940621 ");
 		add(p8);
 
 		// 전송, 취소버튼
+		p9.setBackground(Color.WHITE);
 		bt_trans = new JButton("전송");
+		bt_trans.setForeground(Color.WHITE);
+		bt_trans.setBackground(new Color(0, 102, 51));
 		bt_cancel = new JButton("취소");
+		bt_cancel.setForeground(Color.WHITE);
+		bt_cancel.setBackground(new Color(0, 102, 51));
 		p9.add(bt_trans);
 		p9.add(bt_cancel);
 		add(p9);
@@ -142,7 +233,21 @@ public class JoinForm extends JPanel implements ActionListener {
 		bt_cancel.addActionListener(this);
 		id_bt.addActionListener(this);
 
-		setLayout(new GridLayout(15, 1));
+		setLayout(null);
+		p1.setBounds(0, 0, 400, 55);
+		p_can.setBounds(0, 55, 400, 170);
+		p2.setBounds(10, 250, 380, 40);
+		p3.setBounds(10, 300, 380, 40);
+		p4.setBounds(10, 350, 380, 40);
+		p_can2.setBounds(0, 400, 400, 190);
+		p5.setBounds(10, 600, 380, 40);
+		p6.setBounds(10, 650, 380, 40);
+		p7.setBounds(10, 700, 380, 40);
+		p8.setBounds(10, 750, 380, 40);
+		p9.setBounds(10, 800, 380, 40);
+
+		setPreferredSize(new Dimension(400, 850));
+		setBackground(Color.WHITE);
 		setVisible(false); // 최초에 등장안함
 
 		init();
@@ -279,6 +384,17 @@ public class JoinForm extends JPanel implements ActionListener {
 	private void init() {
 		con = manager.getConnection();
 
+	}
+
+	class MyMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (e.getSource() == id_t) {
+				id_t.setText("");
+			} else if (e.getSource() == birth_t) {
+				birth_t.setText("");
+			}
+		}
 	}
 
 	public void actionPerformed(ActionEvent e) {
--- src/member/LoginForm.java
+++ src/member/LoginForm.java
@@ -2,70 +2,151 @@
 package member;
 
 import java.awt.BorderLayout;
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.text.StyledDocument;
 
 import client.ClientMain;
 import db.DBManager;
 
 public class LoginForm extends JPanel implements ActionListener {
 	MemberWindow memberWindow;
-	JPanel container; // borderLayout 적용
-	JPanel p_center; // grid 를 적용할
+	JPanel container;
+	JPanel p_north;
+	JPanel p_center;
 	JPanel p_south; // 남쪽에 버튼이 들어갈 예정
-	JLabel la_id, la_pw;
+	JLabel la_north, la_center1, la_center2;
 	JTextField t_id;
 	JPasswordField t_pw;
 	JButton bt_login, bt_join;
+	Canvas can_center;
+	BufferedImage image = null;
 
 	public LoginForm(MemberWindow memberWindow) {
 		this.memberWindow = memberWindow;
 		container = new JPanel();
+
+		// north
+		p_north = new JPanel();
+		p_north.setBackground(Color.BLACK);
+		la_north = new JLabel("로그인");
+		la_north.setForeground(Color.WHITE);
+
+		// center
 		p_center = new JPanel();
+		p_center.setBackground(Color.WHITE);
+		la_center1 = new JLabel("안녕하세요. 스타벅스입니다.", JLabel.CENTER);
+		la_center1.setForeground(new Color(156, 136, 86));
+		la_center1.setFont(new Font("돋움", Font.BOLD, 20));
+
+		la_center2 = new JLabel("<html>마이 스타벅스 리워드 회원이 되시면 생일 무료 음료 <br> 쿠폰과 같은 다양한 리워드를 누리실 수 있습니다.<html>",
+				JLabel.CENTER);
+		la_center2.setFont(new Font("돋움", Font.PLAIN, 13));
+
+		// 이미지 url 얻어오기
+		try {
+			URL url = this.getClass().getResource("/main_login_cup01_img.png");
+			image = ImageIO.read(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		can_center = new Canvas() {
+			public void paint(Graphics g) {
+				g.drawImage((Image) image, 178, 0, 42, 90, this);
+
+			}
+		};
+
+		// can_center.setPreferredSize(new Dimension(135, 135));
+
+		t_id = new JTextField("ID입력", 20);
+		t_id.addMouseListener(new MyMouseListener());
+		t_id.setPreferredSize(new Dimension(380, 35));
+		t_pw = new JPasswordField("PW입력", 20);
+		t_pw.addMouseListener(new MyMouseListener());
+		t_pw.setPreferredSize(new Dimension(380, 35));
+
+		// south
 		p_south = new JPanel();
-		la_id = new JLabel("ID");
-		la_pw = new JLabel("Password");
-		t_id = new JTextField("", 15);
-		t_pw = new JPasswordField("", 15);
-		bt_login = new JButton("로그인");
+		p_south.setBackground(Color.WHITE);
+
+		bt_login = new JButton("로그인하기");
+		bt_login.setForeground(Color.WHITE);
+		bt_login.setBackground(new Color(123, 109, 100));
 		bt_join = new JButton("회원가입");
+		bt_join.setForeground(new Color(123, 109, 100));
+		bt_join.setBackground(Color.WHITE);
+		bt_join.setBorderPainted(false);
 
-		container.setLayout(new BorderLayout());
+		// add
+		p_north.add(la_north);
 
-		p_center.setLayout(new GridLayout(2, 2));
-		p_center.add(la_id);
+		p_center.add(can_center);
+		p_center.add(la_center1);
+		p_center.add(la_center2);
 		p_center.add(t_id);
-		p_center.add(la_pw);
 		p_center.add(t_pw);
+
 		p_south.add(bt_login);
 		p_south.add(bt_join);
 
-		container.add(p_center);
+		add(container);
+
+		// 각 component 위치지정
+		container.setLayout(new BorderLayout());
+		container.add(p_north, BorderLayout.NORTH);
+		container.add(p_center, BorderLayout.CENTER);
 		container.add(p_south, BorderLayout.SOUTH);
 
-		add(container);
+		p_north.setPreferredSize(new Dimension(400, 55));
+		p_center.setPreferredSize(new Dimension(400, 465));
+		p_south.setPreferredSize(new Dimension(400, 130));
+
+		p_north.setLayout(null);
+		la_north.setBounds(10, 0, 100, 55);
+
+		p_center.setLayout(null);
+		can_center.setBounds(0, 55, 400, 150);
+		la_center1.setBounds(0, 200, 400, 30);
+		la_center2.setBounds(0, 240, 400, 50);
+		t_id.setBounds(10, 340, 380, 40);
+		t_pw.setBounds(10, 390, 380, 40);
+
+		p_south.setLayout(null);
+		bt_login.setBounds(10, 0, 380, 40);
+		bt_join.setBounds(10, 55, 380, 20);
 
 		// 버튼 연결
 		bt_login.addActionListener(this);
 		bt_join.addActionListener(this);
 
-		setPreferredSize(new Dimension(400, 100));
-		setBackground(Color.PINK);
+		setPreferredSize(new Dimension(400, 650));
 	}
 
 	public void joinCheck() {
@@ -90,7 +171,7 @@ public class LoginForm extends JPanel implements ActionListener {
 		char[] ch = t_pw.getPassword();
 		String pass = new String(ch);
 		if (pass.length() == 0) {
-			JOptionPane.showMessageDialog(this, "비밀번호를 정확히 입력해주세요");
+			JOptionPane.showMessageDialog(this, "비밀번호를 입력해주세요");
 			t_pw.requestFocus();
 			return;
 		}
@@ -141,7 +222,7 @@ public class LoginForm extends JPanel implements ActionListener {
 						memberWindow.page[2].add(new ClientMain(memberWindow));
 						memberWindow.setPage(2);
 					} else {
-						JOptionPane.showMessageDialog(this, "로그인 정보가 올바르지 않습니다.");
+						JOptionPane.showMessageDialog(this, "로그인 정보가 일치하지 않습니다. 아이디나 비밀번호를 확인 후 다시 입력해 주세요");
 					}
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
@@ -168,6 +249,17 @@ public class LoginForm extends JPanel implements ActionListener {
 	// } else {
 	// JOptionPane.showMessageDialog(this, "로그인 정보가 올바르지 않습니다");
 	// }
+
+	class MyMouseListener extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (e.getSource() == t_id) {
+				t_id.setText("");
+			} else if (e.getSource() == t_pw) {
+				t_pw.setText("");
+			}
+		}
+	}
 
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
